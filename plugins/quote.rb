2d1b02f8388f5087fd88e4fbb3d96322a245527f
petru <petrumadar@gmail.com>
2013-07-21 13:58:32 -0700

    Fix quote

--- plugins/quote.rb
+++ plugins/quote.rb
@@ -15,16 +15,16 @@ class Quote
   end
 
   match /^!?(?:quote|q)(:?\s?(\d*)|)$/i, method: :quote, prefix: ""
-  match /^!?(?:findquote|fq)\s?(.*)$/i, method: :find_quote, prefix: ""
+  match /^!?(?:findquote|fq)\s?(.+)$/i, method: :find_quote, prefix: ""
   match /(?:addquote|aq)\s(.*)$/i, method: :add_quote
 
 
   # q, !q or !quote [id]
   def quote(m,id)
     quotes = self.db
-    quote = id.empty? ? Random.rand(quotes.size) : id.to_i-1
-    if quotes[quote]
-      m.reply "[#{quote}/#{quotes.size}] #{quotes[quote]}"
+    quote = id.empty? ? Random.rand(quotes.size) : id.to_i
+    if (0...quotes.size).include? quote
+      m.reply "[#{quote}/#{quotes.size-1}] #{quotes[quote]}"
     else
       m.reply "No such quote. Valid range: 0-#{quotes.size-1}"
     end
@@ -32,7 +32,7 @@ class Quote
 
   # !fq or !findquote <query>
   def find_quote(m,query)
-    quotes = self.db
+    quotes = self.db.shuffle
     quotes.select! { |q| q.include? query }
     if quotes.size > 1
       3.times { |x| m.reply "[#{x+1}/#{quotes.size}] #{quotes[x]}"}
