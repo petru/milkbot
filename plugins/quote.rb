f5cbd167e90c613007ba75091ed641ae3cd8dd8a
petru <petrumadar@gmail.com>
2013-07-21 12:49:28 -0700

    Improve quotes plugin

--- plugins/quote.rb
+++ plugins/quote.rb
@@ -1,11 +1,51 @@
 class Quote
   include Cinch::Plugin
+  @@file = 'quote.txt'
+  @@last_quote = ""
 
-  match /^q$/i, method: :random_quote, prefix: ""
-
-  def random_quote(m)
-    f = open('quote.txt')
-    r = f.read.split("\n")
-    m.reply "#{r.shuffle[0]}"
+  # common methods
+  def db
+    f = open(@@file)
+    quotes = f.read.split("\n")
+    f.close
+    return quotes
   end
+
+  match /^!?(?:quote|q)(:?\s?(\d*)|)$/i, method: :quote, prefix: ""
+  match /^!?(?:findquote|fq)\s?(.*)$/i, method: :find_quote, prefix: ""
+  match /(?:addquote|aq)\s(.*)$/i, method: :add_quote
+
+
+  # q, !q or !quote [id]
+  def quote(m,id)
+    quotes = self.db
+    quote = id.empty? ? Random.rand(quotes.size-1) : id.to_i
+    if quotes[quote]
+      m.reply "[#{quote}/#{quotes.size-1}] #{quotes[quote]}"
+    else
+      m.reply "No such quote. Valid range: 0-#{quotes.size-1}"
+    end
+  end
+
+  # !fq or !findquote <query>
+  def find_quote(m,query)
+    quotes = self.db
+    quotes.select! { |q| q.include? query }
+    if quotes.size > 1
+      3.times { |x| m.reply "[#{x+1}/#{quotes.size}] #{quotes[x]}"}
+    else
+      m.reply quotes[0]
+    end
+  end
+
+  # !aq  or !addquote <text>
+  def add_quote(m,text)
+    file = File.new(@@file, "a")
+    file.write("\n"+text)
+    file.close
+    m.reply "Quote added: #{text}"
+  end
+
+  # !dq or !delquote <id>
+
 end
