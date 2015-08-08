1190b3d8354c6f2ef5b29740c10086284b1ab20d
Petru Madar <petrumadar@yahoo.com>
2015-08-08 16:29:37 +0000

    Refine code

--- plugins/kat.rb
+++ plugins/kat.rb
@@ -4,30 +4,28 @@ require 'uri'
 class Kat
   include Cinch::Plugin
 
-  match /tv (.*) S?(\d{1,2})\DE?(\d{1,2})/i, prefix:".", method: :tv # .tv <showname> S<season>E<episode> OR .tv <showname> <season><episode>
-  match /kat (.*)/i, prefix:".", method: :kat # .kat <anything>
+  match /tv (.*) S?(\d{1,2})\DE?(\d{1,2})$/i, prefix:".", method: :tv # .tv <showname> S<season>E<episode> OR .tv <showname> <season><episode>
+  match /kat (.*)$/i, prefix:".", method: :kat # .kat <anything>
+  
+  def display(m,link)
+    r = open(link)
+    contents = r.read
+    magnet = contents.scan(/href="(magnet\:\?.*)" class="/)    
+    torrent = contents.scan(/<a data-download title="Download torrent file" href="(.*)" class=/)
+    m.reply "#{Format(:bold, "Link:")} #{link}"
+    m.reply "#{Format(:bold, "Magnet:")} #{magnet[0][0]}"
+    m.reply "#{Format(:bold, "Torrent:")} #{torrent[0][0]}"  
+  end
 
   def tv(m,name,season,episode)
     season = "0" + season if season.length == 1
     episode = "0" + episode if episode.length == 1
     link = URI.escape("https://kat.cr/usearch/#{name} S#{season}E#{episode} 720p/?field=seeders&sorder=desc")
-    m.reply "#{Format(:bold, "Link:")} #{link}"
-    r = open(link)
-    contents = r.read
-    magnet = contents.scan(/href="(magnet\:\?.*)" class="/)    
-    torrent = contents.scan(/<a data-download title="Download torrent file" href="(.*)" class=/)
-    m.reply "#{Format(:bold, "Magnet:")} #{magnet[0][0]}"
-    m.reply "#{Format(:bold, "Torrent:")} #{torrent[0][0]}"
+    display(m,link)
   end
 
   def kat(m,query)
     link = URI.escape("https://kat.cr/usearch/#{query}/?field=seeders&sorder=desc")
-    m.reply "#{Format(:bold, "Link:")} #{link}"
-    r = open(link)
-    contents = r.read
-    magnet = contents.scan(/href="(magnet\:\?.*)" class="/)
-    torrent = contents.scan(/<a data-download title="Download torrent file" href="(.*)" class=/)
-    m.reply "#{Format(:bold, "Magnet:")} #{magnet[0][0]}"
-    m.reply "#{Format(:bold, "Torrent:")} #{torrent[0][0]}"
+    display(m,link)
   end
 end
